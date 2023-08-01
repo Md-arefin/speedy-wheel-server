a57c09b1b9760598afb9f8b631b7e44d898dde61
Md-arefin <ansariarefin617@gmail.com>
2023-08-01 15:22:08 +0600

    saved user

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const port = process.env.port || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
 
@@ -11,8 +12,8 @@ app.use(express.json());
 
 // mongoDB Start
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.bqstehg.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bqstehg.mongodb.net/?retryWrites=true&w=majority`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,6 +29,20 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const userCollection = client.db("SpeedyWheel").collection("users");
+        const carCollection = client.db("SpeedyWheel").collection("cars");
+        
+        // user related api
+        app.post("/users", async(req , res) => {
+            const user = req.body;
+            const query = { email: user.email}
+            const existingUser = await userCollection.findOne(query);
+            if(existingUser){
+                return res.send({ message: 'user already exists'})
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
 
         
         // Send a ping to confirm a successful connection
