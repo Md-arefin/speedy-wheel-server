7e3f5a8307d0a8d4984e1becb966799398b21104
Md-arefin <ansariarefin617@gmail.com>
2023-08-10 14:51:37 +0600

    5 commit

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const userCollection = client.db("SpeedyWheel").collection("users");
         const carCollection = client.db("SpeedyWheel").collection("cars");
